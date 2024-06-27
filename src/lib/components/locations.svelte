ee7209ff2432b04fda471792b4fc31e108f2523c
sneakypete81 <sneakypete81@gmail.com>
2024-06-27 22:33:18 +0100

    refactor: locations pane

---
+++ src/lib/components/locations.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { ScrollArea } from '$lib/components/ui/scroll-area/index.js';
+
+	import { listen, type Event } from '@tauri-apps/api/event';
+
+	let branches: string[] = [];
+
+	type Payload = {
+		branches: string[];
+	};
+
+	listen('branches', (event: Event<Payload>) => (branches = event.payload.branches));
+</script>
+
+<div class="font-bold uppercase">Branches</div>
+<ScrollArea class="h-32 h-full">
+	{#each branches as branch}
+		<div class="overflow-clip text-foreground-dim">
+			{branch}
+		</div>
+	{/each}
+</ScrollArea>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,16 @@
 <script lang="ts">
+	import Locations from '$lib/components/locations.svelte';
 	import * as Resizable from '$lib/components/ui/resizable';
-	import { ScrollArea } from '$lib/components/ui/scroll-area/index.js';
 
-	import { listen, type Event } from '@tauri-apps/api/event';
+	import { onMount } from 'svelte';
 	import { invoke } from '@tauri-apps/api/tauri';
 
-	let branches: string[] = [];
-
-	type Payload = {
-		branches: string[];
-	};
-
-	listen('branches', (event: Event<Payload>) => (branches = event.payload.branches));
-
-	invoke('init');
+	onMount(() => invoke('init'));
 </script>
 
 <Resizable.PaneGroup direction="horizontal" class="border">
 	<Resizable.Pane defaultSize={25} class="bg-background-10 pl-2">
-		<div class="font-bold uppercase">Branches</div>
-		<ScrollArea class="h-32 h-full">
-			{#each branches as branch}
-				<div class="overflow-clip text-foreground-dim">
-					{branch}
-				</div>
-			{/each}
-		</ScrollArea>
+		<Locations />
 	</Resizable.Pane>
 	<Resizable.Handle class="bg-background-20" />
 	<Resizable.Pane class="bg-background-20">Two</Resizable.Pane>
