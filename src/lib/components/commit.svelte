25c2e450786a9decf737bb1e4a99c9af0aeb6654
sneakypete81 <sneakypete81@gmail.com>
2024-06-28 13:07:07 +0100

    feat: commit graph line

--- src/lib/components/commit.svelte
+++ src/lib/components/commit.svelte
@@ -10,9 +10,17 @@
 	export let commit: Commit;
 </script>
 
-<div class="p-2">
-	{commit.message}
-	<div class="italic text-foreground-dim">
-		{commit.author}
+<div class="flex">
+	<div class="w-10">
+		<svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 25">
+			<path d="M 10 0 S 10 0, 10 25" class="fill-none stroke-blue-400" />
+			<rect x="7.5" y="10" width="5" height="5" rx="1" class="fill-blue-400" />
+		</svg>
+	</div>
+	<div class="self-center">
+		{commit.message}
+		<div class="italic text-foreground-dim">
+			{commit.author}
+		</div>
 	</div>
 </div>
